dbc26dc7d777aac8f5458341582da1369df4cfff
unknown <hoangdinh37k02@gmail.com>
2022-05-20 17:16:57 +0700

    ss13

---
+++ ss13_string_security/exercise/string_security/src/main/java/com/codegym/StringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringSecurityApplication.class, args);
+    }
+
+}
